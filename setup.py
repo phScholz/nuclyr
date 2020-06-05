104b7719f093b6767625e988cfac247dc3a84973
phScholz <pscholz@outlook.de>
2020-06-05 22:36:13 +0200

    second commit

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='nuclyr',
       version='0.01',
-      description='The funniest joke in the world',
-      url='',
       author='Philipp Scholz',
       author_email='pscholz@outlook.com',
       license='MIT',
       packages=['nuclyr'],
+      url="https://github.com/phScholz/nuclyr",
+      description="A utility package for nuclear data.",
       zip_safe=False)
